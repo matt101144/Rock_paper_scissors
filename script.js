f569b736ead090d91c2d4bd5aa92e1654f48f9c5
matt101144 <matt.neville10@outlook.com>
2024-04-20 00:09:34 +0100

    added to functions 1 to check the winner another to play the game

--- script.js
+++ script.js
@@ -11,6 +11,35 @@ const getComputerChoice = () => {
             return "scissors";
         } 
 }
-console.log(getComputerChoice());
+
 
 // this code above will return rock, paper or scissors at random.
+
+function checkWinner(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper") 
+    ){
+        return `congratulations, ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `you lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+// console.log(checkWinner("paper", getComputerChoice())); 
+
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("pick rock, paper, or scissors!");
+        const computerSelection = getComputerChoice();
+        console.log(checkWinner(playerSelection, computerSelection));
+    }
+}
+    game();
